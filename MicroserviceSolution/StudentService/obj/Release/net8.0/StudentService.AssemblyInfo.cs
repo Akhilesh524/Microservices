e2f5b471ff42d2e3656d34f795b0400d17e33090
Akhilesh Bairwa <a9977134203@gmail.com>
2025-08-26 15:14:30 +0530

    MC-1: add Dto, Services, Repository

--- MicroserviceSolution/StudentService/obj/Release/net8.0/StudentService.AssemblyInfo.cs
+++ MicroserviceSolution/StudentService/obj/Release/net8.0/StudentService.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentService")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1fee79feac43ddc17d7449db9e1ce3a5eefe821")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentService")]
